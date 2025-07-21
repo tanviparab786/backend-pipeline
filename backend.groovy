d9a05fcb9bf96e9e26ff2106a3c2de9230163487
tanviparab <tanviparab786@gmail.com>
2025-07-21 15:41:14 +0530

    pulling code from repository

---
+++ backend.groovy
@@ -0,0 +1,11 @@
+pipeline{
+    agent any
+    stages{
+        stage('pulling code from repository'){
+            steps{
+                git branch: 'main', url: 'https://github.com/tanviparab786/java-backend.git'
+            }
+        }
+        
+    }
+}
